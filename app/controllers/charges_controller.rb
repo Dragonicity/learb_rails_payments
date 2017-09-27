8536783e04f8a1d956ce0a8e02936a1e4723f061
Mark-Nelson <mark.nelson@dragonicity.com>
2017-09-27 10:10:27 +0800

    Added purchases table

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,8 +7,8 @@ class ChargesController < ApplicationController
     # Amount in cents
 
     customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
+      :email       => params[:stripeEmail],
+      :source      => params[:stripeToken]
     )
 
     charge = Stripe::Charge.create(
@@ -18,6 +18,18 @@ class ChargesController < ApplicationController
       :currency    => 'usd'
     )
 
+    purchase = Purchase.create(
+      email:       params[:stripeEmail], 
+      card:        params[:stripeToken],
+      amount:      params[:amount],
+      description: charge.description,
+      currency:    charge.currency,
+      customer_id: customer.id,
+      product_id:  1
+    )
+
+    redirect_to purchase
+    
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
